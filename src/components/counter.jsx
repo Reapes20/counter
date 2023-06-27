e3d33aaf5c834b65e9909ac763eb7dd3b2898b4c
tayshaunk <djtaytaywesgang@icloud.com>
2023-06-27 16:49:58 -0700

    Setup a basic react web app

---
+++ src/components/counter.jsx
@@ -0,0 +1,15 @@
+import { useState } from "react";
+
+const Counter = () => {
+
+    return(
+        <div>
+            <h1></h1>
+            <button> Add </button>
+            <button> subtract </button>
+            <button> Reset </button>
+        </div>
+    )
+};
+
+export default Counter;
