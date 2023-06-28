09cf9b6e68a61cb2ac9d51f594f5d0130e029bd9
TXT 56 <txt.@TXT-56.lan>
2023-06-27 22:49:02 -0700

    this is my prodject

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import './App.css';
 import Greeting from './components/greeting'
 import Logo from './components/Logo';
+import Counter from './components/counter';
 function App() {
-  const Username = "Tayshaun";
+  const Username = "Jonathan Blanks";
   return (
 <div>
   <Greeting name={Username}/>
   <Logo />
+  <Counter />
 </div>
   );
 }
--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -1,13 +1,23 @@
 import { useState } from "react";
 
 const Counter = () => {
+const[count, setCount]=useState(0)
+const countHandler=() => {
+   setCount(count+1) 
+};
+const countTaker=() => {
+    setCount(count-1) 
+ };
+ const countclear=() => {
+    setCount(0) 
+ };
 
     return(
         <div>
-            <h1></h1>
-            <button> Add </button>
-            <button> subtract </button>
-            <button> Reset </button>
+            <h1>{count} </h1>
+            <button onClick={countHandler}> Add </button>
+            <button onClick={countTaker}> Subtract </button>
+            <button onClick={countclear}> Reset </button>
         </div>
     )
 };
